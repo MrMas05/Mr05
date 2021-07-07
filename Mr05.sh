61ca22d6394d2d48a2470e296d64199bafd67efa
MrMas05 <87048156+MrMas05@users.noreply.github.com>
2021-07-07 11:49:34 +0800

    Add files via upload

---
+++ Mr05.sh
@@ -0,0 +1,59 @@
+clear
+blue='\033[34;1m' 
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m' 
+echo
+toilet -f big -F gay By Mr05
+echo $cyan"Welcome To Sc Mr05"
+echo
+echo $blue"[+] AUTHOR:Mr05"
+echo $blue"[+] WHATSSAP:089606521212"
+echo
+sleep 2
+echo $green"silahkan pilih Script nya :"
+echo
+echo $blue"1.) Script Hack CCTV By Storiku"
+echo
+echo $blue"2.) Script Spam Sms Atau Telfon By Mr.H0N3S"
+echo
+echo $red "0.) Exit !!!"
+echo
+echo $green"SILAHKAN BACA PETUNJUK!!!"
+echo
+echo $green"petunjuk: kalian pilih nomer lalu jika sudah kalian download file script nya file download berupa mediafire!! tulisan not found nya jangan di salin"
+echo
+echo $white
+read -p "Pilih Mana : " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Mr05
+echo $yellow"Script Hack CCTV By Storiku"
+sleep 2
+https://www.mediafire.com/file/7gyb8eesjto9yij/SCRIPT+HACK+CCT-WPS+Office.docx/file
+"jika ingin kembali ketik sh Mr05.sh"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay Mr05
+echo $yellow"Script Spam Sms Atau Telfon By Mr.H0N3S"
+sleep 2
+https://www.mediafire.com/file/x639uwbvg7uajb3/SCRIPT_SPAM_SMS-WPS_Office.docx/file
+"jika ingin kembali ketik sh Mr05.sh"
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $red
+figlet "Bye"
+echo $white"To Be Continued"
+sleep 3
+exit
+fi
